13d4333e8d5cf76f944e3af8efa27c883417f192
Garrett Miller <garrettralphmiller@gmail.com>
2016-12-05 14:10:48 -0500

    Updates to index and app.js

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -9,9 +9,12 @@ gulp.task('serve', ['sass'], function() {
         server: "./app"
     });
 
+
     gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
+
+
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -22,3 +25,41 @@ gulp.task('sass', function() {
 });
 
 gulp.task('default', ['serve']);
+
+
+
+
+// SECOND PASTING
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// process JS files and return the stream.
+gulp.task('js', function () {
+    return gulp.src('js/*js')
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('serve', ['js'], function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("js/*.js", ['js-watch']);
+});

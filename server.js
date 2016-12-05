52d015dff0801de53e8791c8fb19af7a3cfb7309
J-C <jcjoneskern@gmail.com>
2016-12-05 14:55:54 -0500

    yelp-fusion node module

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var yelp = require('yelp-fusion');
+
+//need to add these in order for program to work
+var clientId = "";
+var clientSecret = "";
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token);
+
+  client.search({
+    //need to find a way to make this a variable and possibly add other parameters from the search api
+    //like coffee shops or libraries or w/e
+    location: 'detroit'
+  }).then(response => {
+    //these next 2 lines are for express
+    app.get("/api/businesses", function(req, res) {
+      res.send(response.jsonBody);
+    });
+  });
+}).catch(e => {
+  console.log(e);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Example app listening at http://localhost:%s', port);
+});

9c5b93946c6bdc200b1f55a029de77819329c211
Garrett Miller <garrettralphmiller@gmail.com>
2016-12-06 21:29:01 -0500

    remove secrets

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 var yelp = require('yelp-fusion');
 var comment = require("./module");
 var body = require('body');
-var jsonBody = require('body/json')
+var jsonBody = require('body/json');
+var http = require('http');
 
 //fake comments api
 app.get("/api/comments", function(req, res) {
@@ -11,30 +12,31 @@ app.get("/api/comments", function(req, res) {
 });
 
 
-//yelp stuff
-//need to add these in order for program to work
-var clientId = "nXqD_i17OL-LeQYs9dD_og";
-var clientSecret = "iYnxPWwIOgz3NniNxgpmp71DyMOoGUgDrysgb67PC0DzOdy9W8BwWo39tTfUkthX";
+//YELP CALL
+
+    //ERASE BEFORE COMMIT!!!!!!
+    var clientId = "";
+    var clientSecret = "";
+    //ARE THESE ERASED??????
 
 app.get('/api/userSearch', function(req, res) {
-  //begin our api call and grab user search terms from setSearchCtrl
+  console.log("srvrside - setSrchCtrl: ", req.query);
+
+  //define object with userinput to pass as parameters for yelp search
   var searchParams = {
     term: req.query.user_term,
     location: req.query.user_location
   };
-
   console.log(searchParams);
-  console.log("get", req.query);
 
+  //begin call
   yelp.accessToken(clientId, clientSecret).then(response => {
     const client = yelp.client(response.jsonBody.access_token);
 
     client.search(searchParams).then(response => {
-      console.log( "then", searchParams.term)
       console.log("this is yelp's response: ", response.jsonBody.businesses)
 
-        res.send(response.jsonBody);
-
+      res.send(response.jsonBody);
     });
   }).catch(e => {
     console.log(e);

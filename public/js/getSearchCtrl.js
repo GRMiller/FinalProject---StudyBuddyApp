1a39ece1cc1752e850f0b6499c269d30ee803186
J-C <jcjoneskern@gmail.com>
2016-12-08 20:54:48 -0500

    addedded ratings to results view

--- public/js/getSearchCtrl.js
+++ public/js/getSearchCtrl.js
@@ -5,15 +5,17 @@ app.controller("getSearchCtrl", ["$scope", "$http", "searchResultsService", "com
   var displayResults = function() {
     $scope.results = searchResultsService.getSearchResults();
     $scope.ratings = commentsService.getComments(); //maybe rename so you can do .ratings instead on it
-    console.log($scope.results[0].id);
+    // $scope.results = [];
+    console.log($scope.results);
     console.log($scope.ratings);
-    // //some psuedo code for brainstorming
-    // for(var i=0; i < $scope.results.length; i++) {
-    //   if($scope.results[i].id === $scope.ratings.id) {
-    //     //logic for attaching the two
-    //     //new variable?
-    //   }
-    // }
+    for(var i=0; i < $scope.results.length; i++) {
+      for(var j=0; j< $scope.ratings.length; j++) {
+        if($scope.results[i].id === $scope.ratings[j].businessid) {
+          $scope.results[i] = Object.assign($scope.results[i], $scope.ratings[j]);
+          console.log($scope.results[i]);
+        }
+      }
+    }
 
 
   };
